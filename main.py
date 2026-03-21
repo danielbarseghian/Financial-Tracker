18f55ce301b4455d339aa401bf8e47e26944d0ed
Daniel Barseghian <daniel@daniel-deb-laptop.daniel-deb.org>
2026-03-21 12:12:50 +0100

    Only updated a folder by accident

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from Identification.id import Identification
 from random import randint
 from datetime import date
 from colorama import Fore, Style, init
+
 init()
+
 
 def generate_random(n):
     s = ""
@@ -13,64 +15,108 @@ def generate_random(n):
         s = f"{s}{r}"
     return s
 
+
 def get_date():
     return date.today()
+
 
 def login():
     print(Fore.CYAN + "╔══════════════════════════╗")
     print("║        LOGIN MENU        ║")
     print("╠══════════════════════════╣")
-    print("║  " + Fore.RED + "[1] Login to account" + Fore.CYAN + "    ║")
-    print("║  " + Fore.BLUE + "[2] Create an account" + Fore.CYAN + "   ║")
-    print("║  " + Fore.YELLOW + "[3] Quit" + Fore.CYAN + "                ║")
+    print("║  " + Fore.WHITE + "[1] Login to account" + Fore.CYAN + "    ║")
+    print("║  " + Fore.WHITE + "[2] Create an account" + Fore.CYAN + "   ║")
+    print("║  " + Fore.WHITE + "[3] Delete an account" + Fore.CYAN + "   ║")
+    print("║  " + Fore.WHITE + "[4] Quit" + Fore.CYAN + "                ║")
     print("╚══════════════════════════╝" + Style.RESET_ALL)
-    choice = input("  Choose (1-3): ")
 
-    match choice:
-        case "1":
-            return Identification.check_user(input("name: "), input("password: "))
-        case "2":
-            Identification.add_user(input("name: "), input("password: "))
-        case "3":
-            return
+    while True:
+        choice = input("  Choose (1-4): ")
+
+        match choice:
+            case "1":
+                id = input("name: ")
+                if Identification.check_user(id, input("password: ")):
+                    return id
+                else:
+                    print("Account not existante")
+                    continue
+            case "2":
+                while True:
+                    id = input("name: ")
+                    password = input("password: ")
+                    if not Identification.check_user(
+                        id, password
+                    ):  # If user none existante
+                        Identification.add_user(id, password)
+                        return id
+                    else:
+                        print("User already existante")
+            case "3":
+                Identification.remove_user(input("name: "), input("password: "))
+                continue
+            case "4":
+                sys.exit(0)
+            case _:
+                print("Please enter a number from 1 to 4")
+                continue
+
+
 def main():
+    login()
+    print(Fore.MAGENTA + "╔══════════════════════════╗")
+    print("║        LOGIN MENU        ║")
+    print("╠══════════════════════════╣")
+    print("║  " + Fore.WHITE + "[1] Add transaction" + Fore.MAGENTA + "     ║")
+    print("║  " + Fore.WHITE + "[2] Show transactions" + Fore.MAGENTA + "   ║")
+    print("║  " + Fore.WHITE + "[3] Delete a trasaction" + Fore.MAGENTA + " ║")
+    print("║  " + Fore.WHITE + "[4] Quit" + Fore.MAGENTA + "                ║")
+    print("╚══════════════════════════╝" + Style.RESET_ALL)
 
     con = sqlite3.connect("finance.db")
     cur = con.cursor()
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS transactions (
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS transactions (
         id          INTEGER PRIMARY KEY AUTOINCREMENT,
         name        TEXT,
         date        TEXT,
         description TEXT,
         type        TEXT,
         amount      REAL
-    )''')
+    )"""
+    )
 
     while True:
-        match input().lower().strip():
+        ch = input("Choose (1-4): ").strip().lower()
+        match ch:
 
-            case "add":
+            case "1" | "delete":
 
-                cur.execute('''INSERT OR IGNORE INTO transactions VALUES(?, ?, ?, ?, ?, ?)''',(
-                            generate_random(10),
-                            input("What is the name of the pushace? "),
-                            get_date(),
-                            input("Short description of the pushace. "),
-                            input("What type of pushace was this? "),
-                            input("How much was this transaction? ")))
-                
-            case "show":
+                cur.execute(
+                    """INSERT OR IGNORE INTO transactions VALUES(?, ?, ?, ?, ?, ?)""",
+                    (
+                        generate_random(10),
+                        input("What is the name of the pushace? "),
+                        get_date(),
+                        input("Short description of the pushace. "),
+                        input("What type of pushace was this? "),
+                        input("How much was this transaction? "),
+                    ),
+                )
 
-                for row in cur.execute('''SELECT * FROM transactions'''):
+            case "2" | "show":
+
+                for row in cur.execute("""SELECT * FROM transactions"""):
                     print(row)
-            
-            case "delete":
-                
+
+            case "3" | "delete":
+
                 d = input("What do you want to delete? ")
                 cur.execute("DELETE FROM transactions WHERE name = ?", (d,))
 
-            case "quit":
+            case "4" | "quit":
+
                 sys.exit(0)
 
             case _:
@@ -78,4 +124,5 @@ def main():
 
         con.commit()
 
-print(login())
+
+main()
